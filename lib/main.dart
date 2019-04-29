b7236bd116fc2f5df80bf6209522e454ac5b175f
nichanan <nichanan@nichanans-MacBook-Pro.local>
2019-04-29 14:47:26 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,115 +5,109 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import './models/json_model.dart';
 import 'dart:convert';
+
 void main() {
- runApp(App());
+  runApp(App());
 }
 
 class App extends StatelessWidget {
- @override
- Widget build(BuildContext context) {
-   return MaterialApp(
-     title: 'Sun Authen',
-     home: HomePage(),
-   );
- }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sun Authen',
+      home: HomePage(),
+    );
+  }
 }
 
 class HomePage extends StatefulWidget {
- @override
- _HomePageState createState() => _HomePageState();
+  @override
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
- final formKey = GlobalKey<FormState>();
- final _scaffold = GlobalKey<ScaffoldState>();
+  final formKey = GlobalKey<FormState>();
+  final _scaffold = GlobalKey<ScaffoldState>();
 
- String barcode = "";
+  String barcode = "";
 
- @override
- Widget build(BuildContext context) {
-   return Scaffold(
-     key: _scaffold,
-     appBar: AppBar(
-       title: Text('QR Counter'),
-      //  actions: <Widget>[IconButton(tooltip: 'upload',
-      //    icon: Icon(Icons.cloud_upload),onPressed: (){},)],
-     ),
-     body: Form(
-       key: formKey,
-       child: ListView(
-         children: <Widget>[
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffold,
+      appBar: AppBar(
+        title: Text('QR Counter'),
+      ),
+      body: Form(
+        key: formKey,
+        child: ListView(
+          children: <Widget>[
+            
+            Text(barcode),
+            
+            Container(
+              margin: EdgeInsets.only(left: 50.0, right: 50.0, top: 50.0),
+              child: Row(
+                children: <Widget>[
+                  new Expanded(
+                    child: signUpButton(context),
+                  ),
+                  // new Expanded(
 
+                  //   child: signaturepad(context),),
 
-           Container(
-             margin: EdgeInsets.only(left: 50.0, right: 50.0),
-             child: Row(
-               children: <Widget>[
-                 new Expanded(
-                   child: signUpButton(context),
-                 ),
-                 //new Expanded(child: Container(child: testText(),)
-               ],
-             ),
-           ),
+                  //newF Expanded(child: Container(child: testText(),)
+                ],
+              ),
+            ),
+            
+          ],
+        ),
+      ),
+      floatingActionButton:
+          FloatingActionButton(onPressed: scan, child: Icon(Icons.add_a_photo)),
+    );
+  }
 
-           Text(barcode),
+  Widget signUpButton(BuildContext context) {
+    return RaisedButton(
+      color: Colors.green,
+      child: Text(
+        'confirm',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {
+        print('your click SignIn');
+        var myRounte = new MaterialPageRoute(
+            builder: (BuildContext context) => Register());
+        Navigator.of(context).push(myRounte);
+      },
+    );
+  }
 
-         ],
-       ),
-     ),
-     floatingActionButton: FloatingActionButton(
-       onPressed: scan,
-         child: Icon(Icons.add_a_photo)),
+  Future sendtoserver(data) async {
+    print('============');
+    print(data);
+    print('============');
 
+    var url = 'http://androidthai.in.th/sun/addDataOill.php';
+    var response =
+        await http.post(url, body: {'isAdd': 'true', 'Barcode': data});
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+  }
 
-   );
+  Future scan() async {
+    try {
+      String barcode = await BarcodeScanner.scan();
+      setState(() => this.barcode = barcode);
 
-
- }
-
- Widget signUpButton(BuildContext context) {
-   return RaisedButton(
-     color: Colors.orange,
-     child: Text(
-       'confirm',
-       style: TextStyle(color: Colors.white),
-     ),
-     onPressed: () {
-       print('your click SignIn');
-       var myRounte = new MaterialPageRoute(
-           builder: (BuildContext context) => Register());
-       Navigator.of(context).push(myRounte);
-     },
-   );
- }
-
- Future sendtoserver(data) async {
-   print('============');
-   print(data);
-   print('============');
-
-   var url = 'http://androidthai.in.th/sun/addDataOill.php';
-   var response = await http.post(url, body: {'isAdd': 'true', 'Barcode': data});
-   print('Response status: ${response.statusCode}');
-   print('Response body: ${response.body}');
- }
-
- Future scan() async {
-   try {
-     String barcode = await BarcodeScanner.scan();
-     setState(() => this.barcode = barcode);
-
-     sendtoserver(this.barcode);
-   } on FormatException{
-      setState(() => this.barcode = 'null (User returned using the "back"-button before scanning anything. Result)');
-   } catch (e) {
-     setState(() => this.barcode = 'Unknown error: $e');
-   }
- }
-
-
+      sendtoserver(this.barcode);
+    } on FormatException {
+      setState(() => this.barcode =
+          'null (User returned using the "back"-button before scanning anything. Result)');
+    } catch (e) {
+      setState(() => this.barcode = 'Unknown error: $e');
+    }
+  }
 } //_HomePageState
-
-
-
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -2,130 +2,245 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
 import 'dart:convert';
 import '../main.dart';
+import '../screen2/signature.dart';
+import 'package:flutter_signature_pad/flutter_signature_pad.dart';
 
 class Register extends StatefulWidget {
- final Widget child;
+  final Widget child;
 
- Register({Key key, this.child}) : super(key: key);
+  Register({Key key, this.child}) : super(key: key);
 
- _RegisterState createState() => _RegisterState();
+  _RegisterState createState() => _RegisterState();
+
+  
 }
 
 class _RegisterState extends State<Register> {
- final formKey = GlobalKey<FormState>();
- String licenString = '';
- String transString = '';
- String nameString = '';
+  final formKey = GlobalKey<FormState>();
+  String nameString = '';
+  String licenString = '';
+  String transString = '';
+  List<Offset> _points = <Offset>[];
 
- @override
- Widget build(BuildContext context) {
-   return Scaffold(
-       appBar: AppBar(
-         title: Text('Confirm'),
-         actions: <Widget>[
-           IconButton(
-             tooltip: 'Upload To Server',
-             icon: Icon(Icons.cloud_upload),
-             onPressed: () {
-               uploadToServer();
-             },
-           )
-         ],
-       ),
-       body: Form(
-         key: formKey,
-         child: ListView(
-           children: <Widget>[
-             Container(
-               margin: EdgeInsets.only(left: 50.0, right: 50.0, top: 50.0),
-               child: licenTextField(),
-             ),
-             Container(
-               margin: EdgeInsets.only(left: 50.0, right: 50.0),
-               child: transTextField(),
-             ),
-             Container(
-               margin: EdgeInsets.only(left: 50.0, right: 50.0),
-               child: nameTextField(),
-             )
-           ],
-         ),
-       ));
- }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('ยืนยันขนส่ง'),
+          actions: <Widget>[
+            IconButton(
+              tooltip: 'Upload To Server',
+              icon: Icon(Icons.cloud_upload),
+              onPressed: () {
+                uploadToServer();
+              },
+            )
+          ],
+        ),
+        body: Form(
+          key: formKey,
+          child: ListView(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(left: 50.0, right: 50.0, top: 20.0),
+                child: licenTextField(),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 50.0, right: 50.0, top: 20),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7.0)),
+                      color: Colors.yellow,
+                      textColor: Colors.red,
+                      onPressed: () {
+                        print('DHL');
+                      },
+                      child: const Text('DHL'),
+                    ),
+                    RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7.0)),
+                      color: Colors.orange,
+                      textColor: Colors.green,
+                      onPressed: () {
+                        print('ALPHA');
+                      },
+                      child: const Text('ALPHA'),
+                    ),
+                    RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7.0)),
+                      onPressed: () {
+                        print('FLASH');
+                      },
+                      color: Colors.black,
+                      textColor: Colors.yellow,
+                      padding: const EdgeInsets.all(0.0),
+                      child: Text('FLASH'),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 50.0, right: 50.0, top: 7.0),
+                child: transTextField(),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 50.0, right: 50.0,top: 7.0),
+                child: nameTextField(),
+              ),
+              
+              Container(
+              
+                margin: EdgeInsets.all(20.0),
+                padding: EdgeInsets.all(10.0),
+                alignment: Alignment.topCenter,
+                width: 200,
+                height: 210,
+                color: Colors.blue[100],
+                child: new GestureDetector(
+          onPanUpdate: (DragUpdateDetails details) {
+            setState(() {
+              RenderBox object = context.findRenderObject();
+              Offset _localPosition =
+                  object.globalToLocal(details.globalPosition);
+              _points = new List.from(_points)..add(_localPosition);
+            });
+          },
+          onPanEnd: (DragEndDetails details) => _points.add(null),
+          child: new CustomPaint(
+            painter: new Signature(points: _points),
+            size: Size.infinite,
+          ),
+        ),
+      ),
+              
 
- void uploadToServer() {
-//    String licenString = '';
-//  String transString = '';
-//  String nameString = '';
-   print('You Click Upload');
-   // formKey.currentState.reset();
-   print(formKey.currentState.validate());
-   formKey.currentState.save();
-   print('Licen = $licenString, Trans = $transString, Name = $nameString');
-   sentNewUserToServer(licenString, transString, nameString);
-   
- }
+              Container(
+                margin: EdgeInsets.only(left: 50.0, right: 50.0, top: 20.0),
+                
+                child: Row(
+                  children: <Widget>[
+                    new Expanded(
+                      child: signaturepad(context),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
 
- void sentNewUserToServer(
-  String userLicen, String userTrans, String userName) async {
-    print('Licen = $userLicen, Trans = $userTrans, Name = $userName');
-  String url ='http://androidthai.in.th/sun/addUserOil.php?isAdd=true&Licen=$userLicen&Trans=$userTrans&Name=$userName';
-   print(url);
-   var respone = await get(url);
-   print(respone);
-   var result = json.decode(respone.body);
-   print(result);
-   print('result ==>$result');
-   if (result.toString() == 'true') {
+  Widget signaturepad(BuildContext context) {
+    return RaisedButton(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7.0)),
+      
+      color: Colors.blue,
+      child: Text(
+        'ลายเซนต์',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {
+        print('your click signature');
+        var myRounte = new MaterialPageRoute(
+            builder: (BuildContext context) => Signaturpad());
+        Navigator.of(context).push(myRounte);
+      },
+    );
+  }
 
-     print('back process');
-     Navigator.pop(context);
+  void uploadToServer() {
+    print('You Click Upload');
+    // formKey.currentState.reset();
+    print(formKey.currentState.validate());
+    formKey.currentState.save();
+    print('Licen = $licenString, Trans = $transString, Name = $nameString');
+    sentNewUserToServer(licenString, transString, nameString);
+  }
 
-     //var backRount =new MaterialPageRoute(builder: (BuildContext )=> HomePage());
-      //Navigator.of(context).push(backRount);
+  void sentNewUserToServer(
+      String userLicen, String userTrans, String userName) async {
+    String url =
+        'http://androidthai.in.th/sun/addUserNich.php?isAdd=true&Licen=$userLicen&Trans=$userTrans&Name=$userName';
+    var respone = await get(url);
+    var result = json.decode(respone.body);
+    print('result ==>$result');
+    if (result.toString() == 'true') {
+      print('back process');
+      Navigator.pop(context);
 
-   }
- }
+      // var backRount =new MaterialPageRoute(builder: (BuildContext )=> HomePage());
+      // Navigator.of(context).push(backRount);
 
- Widget licenTextField() {
-   return TextFormField(
-     decoration: InputDecoration(labelText: 'licen:', hintText: 'Name Only'),
-     validator: (String value) {
-       if (value.length == 0) {
-         return 'licen not Blank ?';
-       }
-     },
-     onSaved: (String licen) {
-       licenString = licen;
-     },
-   );
- }
-Widget transTextField() {
-   return TextFormField(
-     decoration: InputDecoration(labelText: 'trans:', hintText: 'Name Only'),
-     validator: (String value) {
-       if (value.length == 0) {
-         return 'trans not Blank ?';
-       }
-     },
-     onSaved: (String trans) {
-       transString = trans;
-     },
-   );
- }
+    }
+  }
 
- Widget nameTextField() {
-   return TextFormField(
-     decoration: InputDecoration(labelText: 'Name:', hintText: 'Name Only'),
-     validator: (String value) {
-       if (value.length == 0) {
-         return 'name not Blank ?';
-       }
-     },
-     onSaved: (String name) {
-       nameString = name;
-     },
-   );
- }
+  Widget licenTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'ทะเบียนรถ:',
+      ),
+      validator: (String value) {
+        if (value.length == 0) {
+          return 'licen not Blank ?';
+        }
+      },
+      onSaved: (String licen) {
+        licenString = licen;
+      },
+    );
+  }
+
+  Widget transTextField() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'ขนส่ง:'),
+      validator: (String value) {
+        if (value.length == 0) {
+          return 'trans not Blank ?';
+        }
+      },
+      onSaved: (String trans) {
+        transString = trans;
+      },
+    );
+  }
+
+  Widget nameTextField() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'ผู้รับ:'),
+      validator: (String value) {
+        if (value.length == 0) {
+          return 'Name not Blank ?';
+        }
+      },
+      onSaved: (String name) {
+        nameString = name;
+      },
+    );
+  }
 }
+class Signature extends CustomPainter {
+  List<Offset> points;
 
+  Signature({this.points});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = new Paint()
+      ..color = Colors.blue
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 10.0;
+
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null) {
+        canvas.drawLine(points[i], points[i + 1], paint);
+      }
+    }
+  }
+  @override
+  bool shouldRepaint(Signature oldDelegate) => oldDelegate.points != points;
+}
